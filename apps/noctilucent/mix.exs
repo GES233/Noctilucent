1cd91af6c4a365f733f3726fb5e1ec27fb461072
RoastChestnut <chestnut.roasted@outlook.com>
2024-09-30 22:40:49 +0800

    使 Mix 可以找到 docs()

--- apps/noctilucent/mix.exs
+++ apps/noctilucent/mix.exs
@@ -13,7 +13,8 @@ defmodule Noctilucent.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
