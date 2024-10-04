395bbbf74a210e90e747142af7e8c34a25bb1802
John Smith <admin@meowcave.top>
2024-10-04 11:55:50 +0800

    规避了 Inspect protocol consolidation 报错

--- apps/noctilucent/mix.exs
+++ apps/noctilucent/mix.exs
@@ -14,7 +14,8 @@ defmodule Noctilucent.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
-      docs: docs()
+      # https://elixirforum.com/t/the-inspect-protocol-has-already-been-consolidated-for-ecto-schema-with-redacted-field/34992/8
+      consolidate_protocols: Mix.env() != :dev
     ]
   end
 
