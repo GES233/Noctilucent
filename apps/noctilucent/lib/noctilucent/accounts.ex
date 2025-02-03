29201fdf4650a51f934d3216fc9919ca52c00c21
RoastChestnut <chestnut.roasted@outlook.com>
2025-02-03 23:53:24 +0800

    重构用户信息修改逻辑，简化代码并引入通用操作；更新用户名、昵称和信息的处理方式

--- apps/noctilucent/lib/noctilucent/accounts.ex
+++ apps/noctilucent/lib/noctilucent/accounts.ex
@@ -78,7 +78,7 @@ defmodule Noctilucent.Accounts do
 
   ## Examples
 
-      iex> register_user(%AuditLog{}, %{field: value})
+      iex> register_user(%AuditLog{}, %{username: "jntm", password: "cxk0802"})
       {:ok, %User{}}
 
       iex> register_user(%AuditLog{}, %{field: bad_value})
@@ -107,30 +107,18 @@ defmodule Noctilucent.Accounts do
 
   这个可能需要改，我的期望是用户名的更改需要重新
   确认用户身份（也就是重新输入密码）以及时间限制。
+  但还是先这样吧。
   """
   def do_change_username(%{user: user} = audit_log, username) do
-    changeset =
-      user
-      |> User.username_changeset(%{username: username})
-
-    Ecto.Multi.new()
-    |> Ecto.Multi.update(:user, changeset)
-    |> AuditLog.multi(
-      audit_log,
-      :account,
-      "user.update_username",
-      %{user_id: user.id, username: username, old_username: user.username}
-    )
-    |> Repo.transaction()
-    |> case do
-      {:ok, %{user: user}} -> {:ok, user}
-      {:error, :user, changeset, _} -> {:error, changeset}
-    end
+    user
+    |> User.username_changeset(%{username: username})
+    |> update_when_userinfo(audit_log, user, "user.update_username", username)
   end
 
   # change_current/2
   def change_user_current(user, current) do
-    User.current_changeset(user, %{current: current})
+    user
+    |> User.current_changeset(%{current: current})
     |> Repo.update()
 
     # [TODO) 上 AuditLog
@@ -138,56 +126,63 @@ defmodule Noctilucent.Accounts do
 
   # change_nickname/2
   def change_user_nickname(%{user: user} = audit_log, nickname) do
-    changeset =
-      user
-      |> User.nickname_changeset(%{nickname: nickname})
-
-    Ecto.Multi.new()
-    |> Ecto.Multi.update(:user, changeset)
-    |> AuditLog.multi(
-      audit_log,
-      :account,
-      "user.update_info.nickname",
-      %{user_id: user.id, nickname: nickname}
-    )
-    |> Repo.transaction()
-    |> case do
-      {:ok, %{user: user}} -> {:ok, user}
-      {:error, :user, changeset, _} -> {:error, changeset}
-    end
+    user
+    |> User.nickname_changeset(%{nickname: nickname})
+    |> update_when_userinfo(audit_log, user, "user.update_info.nickname", nickname)
   end
 
   # change_info/2
   def change_user_info(%{user: user} = audit_log, info_content) do
-    changeset =
-      user
-      |> User.info_changeset(%{info: info_content})
-
-    Ecto.Multi.new()
-    |> Ecto.Multi.update(:user, changeset)
-    |> AuditLog.multi(
-      audit_log,
-      :account,
-      "user.update_info.info",
-      %{user_id: user.id, info: info_content}
-    )
-    |> Repo.transaction()
-    |> case do
-      {:ok, %{user: user}} -> {:ok, user}
-      {:error, :user, changeset, _} -> {:error, changeset}
-    end
+    user
+    |> User.info_changeset(%{info: info_content})
+    |> update_when_userinfo(audit_log, user, "user.update_info.info", info_content)
   end
 
   # change_gender/2
   def change_user_gender(_user, _gender) do
     raise Helpers.NotImplement
-    # [TODO) 上 AuditLog
+    # [TODO) 写完 changeset
   end
 
   # change_user_gender_visibility/2
   def change_user_gender_visibility(_user, _visible) do
+    # 这个不用上 AuditLog
     raise Helpers.NotImplement
-    # [TODO) 上 AuditLog
+    # [TODO) 写完 changeset
+  end
+
+  # 【用户】主动修改个人信息的通用操作
+  defp update_when_userinfo(user_changeset, audit_log, old_user, verb, new_item) do
+    new_context = case verb do
+      "user.update_username" ->
+        %{
+          user_id: old_user.id,
+          username: new_item,
+          old_username: old_user.username
+        }
+      "user.update_info.nickname" ->
+        %{
+          user_id: old_user.id,
+          nickname: new_item
+        }
+      "user.update_info.info" ->
+        %{
+          user_id: old_user.id,
+          info: new_item
+        }
+      # current
+      # gender
+      # gender_visibility
+    end
+
+    Ecto.Multi.new()
+    |> Ecto.Multi.update(:user, changeset)
+    |> AuditLog.multi(audit_log, :account, verb, new_context)
+    |> Repo.transaction()
+    |> case do
+      {:ok, %{user: user}} -> {:ok, user}
+      {:error, :user, changeset, _} -> {:error, changeset}
+    end
   end
 
   # 这个需要 attrs 吗？
