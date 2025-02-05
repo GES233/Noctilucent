312f720d86880a674562d4466119715787e823ec
RoastChestnut <chestnut.roasted@outlook.com>
2025-02-05 13:01:33 +0800

    更新 ROADMAP，添加头像相关内容；在用户展示逻辑中支持通过用户名获取用户信息

--- apps/noctilucent_web/lib/noctilucent_web/live/user_live/show.ex
+++ apps/noctilucent_web/lib/noctilucent_web/live/user_live/show.ex
@@ -9,11 +9,19 @@ defmodule NoctilucentWeb.UserLive.Show do
   end
 
   @impl true
+  # id 太拟人了，后面换成 username
   def handle_params(%{"id" => id}, _, socket) do
     {:noreply,
      socket
      |> assign(:page_title, page_title(socket.assigns.live_action))
      |> assign(:user, Accounts.get_user!(id))}
+  end
+
+  def handle_params(%{"username" => username}, _, socket) do
+    {:noreply,
+     socket
+     |> assign(:page_title, page_title(socket.assigns.live_action))
+     |> assign(:user, Accounts.get_user_from_username(username))}
   end
 
   defp page_title(:show), do: "Show User"
