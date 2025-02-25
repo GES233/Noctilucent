1244c2ab12b3ab559073ebc561ebc427f1aa5407
awaChestnut <chestnut.roasted@outlook.com>
2025-02-25 09:24:30 +0800

    [Core][test] 新增审计日志上下文的无效错误处理，更新审计日志插入逻辑及测试用例

--- apps/noctilucent/lib/noctilucent/audit_log.ex
+++ apps/noctilucent/lib/noctilucent/audit_log.ex
@@ -3,6 +3,29 @@ defmodule Noctilucent.AuditLog.Context do
   负责处理日志的上下文。
   """
 
+  defmodule InvalidError do
+    @moduledoc """
+    当上下文不符合要求时抛出的异常。
+    """
+
+    defexception [:message]
+
+    # 这个可以细一点
+    # 检查给定的键所对应的上下文与输入有哪些不同
+    @impl true
+    def exception(term) do
+      msg = case term do
+        {:missing, missing_fields} ->
+          "Missing fields: #{inspect(missing_fields)}"
+
+        {:extra, extra_fields} ->
+          "Extra fields: #{inspect(extra_fields)}"
+      end
+
+      %InvalidError{message: msg}
+    end
+  end
+
   # 保存的键，对于值的读取需要对应的函数来操作
   @params %{
     # 用户自发和账号相关的动作
@@ -23,7 +46,7 @@ defmodule Noctilucent.AuditLog.Context do
       # 用户冻结
       "user.freeze" => ~w(exp_expire_time),
       # 删除账号
-      "user.delete_account" => ~w(),
+      "user.delete_account" => ~w(username)
     },
     # 社交相关
     social: %{
@@ -37,7 +60,7 @@ defmodule Noctilucent.AuditLog.Context do
       "user.contact.remove" => ~w(user_id contact_id),
       # 拉黑/解除拉黑
       "user.block" => ~w(user_id block_id),
-      "user.unblock" => ~w(user_id block_id),
+      "user.unblock" => ~w(user_id block_id)
     },
     # 管理员的动作
     moderator: %{
@@ -58,7 +81,7 @@ defmodule Noctilucent.AuditLog.Context do
       # 点赞点踩
       "responce.like" => ~w(user_id content_type content_id),
       "responce.dislike" => ~w(user_id content_type content_id),
-      "responce.natural" => ~w(user_id content_type content_id),
+      "responce.natural" => ~w(user_id content_type content_id)
       # 评论相关
     },
     # 房间相关
@@ -70,7 +93,9 @@ defmodule Noctilucent.AuditLog.Context do
       # 踢出/加入房间黑名单
     },
     # 系统自动执行的动作
-    # noctilucent: %{}
+    noctilucent: %{
+      "" => []
+    }
   }
 
   @doc """
@@ -79,6 +104,7 @@ defmodule Noctilucent.AuditLog.Context do
   for {scope, actions_map} <- @params, {verb, action} <- actions_map do
     def by_scope_and_verb(unquote(scope), unquote(verb)), do: {:ok, unquote(action)}
   end
+
   def by_scope_and_verb(_scope, _verb), do: {:error, :not_found}
 
   @doc """
@@ -87,16 +113,13 @@ defmodule Noctilucent.AuditLog.Context do
   for {scope, actions_map} <- @params do
     def by_scope(unquote(scope)), do: {:ok, unquote(Macro.escape(actions_map))}
   end
+
   def by_scope(_), do: {:error, :not_found}
 end
 
 defmodule Noctilucent.AuditLog do
   @moduledoc """
   类似于「岁月史书」的功能。
-
-  *就怕有人搞事儿，等记住 IP 地址，连着逆天言行发给学校就老实了。*
-
-  不抖机灵了，这种记录功能还是挺必要的。
 
   ### 格式说明
 
@@ -107,8 +130,10 @@ defmodule Noctilucent.AuditLog do
   的类别以及 ID 或者是相关的数据，在操作时需要被检查或验证。
   """
   use Ecto.Schema
+
   import Ecto.Query
   import Ecto.Changeset
+  alias Noctilucent.Repo
 
   # 说实话，这块我没抄明白
   schema "audit_logs" do
@@ -129,6 +154,13 @@ defmodule Noctilucent.AuditLog do
     audit_log
     |> cast(attrs, [:verb, :scope, :ip_addr, :user_agent, :context])
     |> validate_required([:verb, :scope, :ip_addr, :user_agent])
+  end
+
+  @doc """
+  插入一条日志。
+  """
+  def audit!(audit_context, scope, verb, context) do
+    Repo.insert!(build!(audit_context, scope, verb, context))
   end
 
   @doc """
@@ -191,22 +223,24 @@ defmodule Noctilucent.AuditLog do
     |> validate_context!()
   end
 
-  # [TODO) 调用相关模块实现检查功能
-  # alias Noctilucent.AuditLog.Context
+  # 调用相关模块实现检查功能
+  alias Noctilucent.AuditLog.Context
 
-  defp validate_context!(struct) do
-    # 这边一旦出了问题，一般是开发者设计的锅
-    # 直接 raise 就好
+  defp validate_context!(%__MODULE__{scope: scope, verb: verb, context: context} = struct) do
+    with {:ok, valid_context} <- Context.by_scope_and_verb(scope, verb),
+         actual_context <- context |> Map.keys() |> Enum.map(&to_string/1),
+         {[], []} <- {actual_context -- valid_context, valid_context -- actual_context} do
+      :ok
+    else
+      {:error, _} -> raise "Invalid scope and verb"
+
+      {missing = [_ | _], _} ->
+        raise Context.InvalidError, {:missing, missing}
+
+      {_, extra} ->
+        raise Context.InvalidError, {:extra, extra}
+    end
+
     struct
   end
-
-  # defp has_correct_context_with_scope_and_verb(context, scope, verb) do
-  #   with
-  #       {:ok, action} <- Noctilucent.AuditLog.Context.by_scope_and_verb(scope, verb),
-  #       Enum.same?(Map.keys(context), action) do
-  #     {:ok, context}
-  #   else
-  #     {:error, reason} -> {:error, reason}
-  #   end
-  # end
 end
--- apps/noctilucent/test/noctilucent/audit_log_test.exs
+++ apps/noctilucent/test/noctilucent/audit_log_test.exs
@@ -2,5 +2,38 @@ defmodule Noctilucent.AuditLogTest do
   # 涉及到数据库的，无脑引入就行了
   use Noctilucent.DataCase
 
-  # alias Noctilucent.AuditLog
+  import Noctilucent.AccountsFixtures
+  alias Noctilucent.AuditLog
+
+  describe "audit!/3" do
+    # ...
+
+    test "检验参数" do
+      audit_log = %AuditLog{ip_addr: {127, 0, 0, 1}, user_agent: ""}
+
+      assert_raise AuditLog.Context.InvalidError, ~r/username/, fn ->
+        AuditLog.audit!(audit_log, :account, "user.sign_up", %{})
+      end
+    end
+  end
+
+  describe "multi/5" do
+    test "通过参数实现" do
+      # ...
+    end
+
+    test "通过回调函数实现" do
+      # ...
+    end
+  end
+
+  describe "查询" do
+    setup do
+      _user = user_fixture()
+
+      # TODO: 插入一系列事务
+    end
+
+    # ...
+  end
 end
